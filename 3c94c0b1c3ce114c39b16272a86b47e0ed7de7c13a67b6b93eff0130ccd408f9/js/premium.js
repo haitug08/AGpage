d6bac3d4e6d5364cd2837321ca7724d4c7727c6f
haitug08 <haitug08@gmail.com>
2025-01-17 16:40:46 +0900

    Update premium.js

--- 3c94c0b1c3ce114c39b16272a86b47e0ed7de7c13a67b6b93eff0130ccd408f9/js/premium.js
+++ 3c94c0b1c3ce114c39b16272a86b47e0ed7de7c13a67b6b93eff0130ccd408f9/js/premium.js
@@ -14,49 +14,49 @@ function calculate() {
   if (classValue === "1") { // 個人事業主
     if (planValue === "1") { // ライトプラン
       premium = timesValue === "1" ? 4500 : 54000; // 月払か年払
-      legalConsultationText = "5.5万円・2回/年";
-      attorneyFeeText = "25万円・2回/年";
+      legalConsultationText = "5.5万円・2回/年・基本てん補割合100％";
+      attorneyFeeText = "25万円・2回/年・基本てん補割合100％";
       specialClauseText = "弁護士費用保険金（報酬金等対応分）不担保特約・代表者個人補償特約";
     } else if (planValue === "2") { // スタンダードプラン
       premium = timesValue === "1" ? 11700 : 140400;
-      legalConsultationText = "5.5万円・2回/年";
-      attorneyFeeText = "50万円・2回/年";
+      legalConsultationText = "5.5万円・2回/年・基本てん補割合100％";
+      attorneyFeeText = "50万円・2回/年・基本てん補割合100％";
       specialClauseText = "代表者個人補償特約";
     }
   } else if (classValue === "2") { // 法人A
     if (planValue === "1") {
       premium = timesValue === "1" ? 26500 : 318000;
-      legalConsultationText = "11万円・3回/年";
-      attorneyFeeText = "300万円・3回/年";
+      legalConsultationText = "11万円・3回/年・基本てん補割合100％";
+      attorneyFeeText = "300万円・3回/年・基本てん補割合100％";
       specialClauseText = "弁護士費用保険金（報酬金等対応分）不担保特約・代表者個人補償特約";
     } else if (planValue === "2") {
       premium = timesValue === "1" ? 63900 : 766800;
-      legalConsultationText = "11万円・3回/年";
-      attorneyFeeText = "600万円・3回/年";
+      legalConsultationText = "11万円・3回/年・基本てん補割合100％";
+      attorneyFeeText = "600万円・3回/年・基本てん補割合100％";
       specialClauseText = "代表者個人補償特約";
     }
   } else if (classValue === "3") { // 法人B
     if (planValue === "1") {
       premium = timesValue === "1" ? 22000 : 264000;
-      legalConsultationText = "11万円・3回/年";
-      attorneyFeeText = "100万円・3回/年";
+      legalConsultationText = "11万円・3回/年・基本てん補割合100％";
+      attorneyFeeText = "100万円・3回/年・基本てん補割合100％";
       specialClauseText = "弁護士費用保険金（報酬金等対応分）不担保特約・代表者個人補償特約";
     } else if (planValue === "2") {
       premium = timesValue === "1" ? 52800 : 633600;
-      legalConsultationText = "11万円・3回/年";
-      attorneyFeeText = "200万円・3回/年";
+      legalConsultationText = "11万円・3回/年・基本てん補割合100％";
+      attorneyFeeText = "200万円・3回/年・基本てん補割合100％";
       specialClauseText = "代表者個人補償特約";
     }
   } else if (classValue === "4") { // 法人C
     if (planValue === "1") {
       premium = timesValue === "1" ? 10000 : 120000;
-      legalConsultationText = "11万円・3回/年";
-      attorneyFeeText = "100万円・3回/年";
+      legalConsultationText = "11万円・3回/年・基本てん補割合100％";
+      attorneyFeeText = "100万円・3回/年・基本てん補割合100％";
       specialClauseText = "弁護士費用保険金（報酬金等対応分）不担保特約・代表者個人補償特約";
     } else if (planValue === "2") {
       premium = timesValue === "1" ? 22800 : 273600;
-      legalConsultationText = "11万円・3回/年";
-      attorneyFeeText = "200万円・3回/年";
+      legalConsultationText = "11万円・3回/年・基本てん補割合100％";
+      attorneyFeeText = "200万円・3回/年・基本てん補割合100％";
       specialClauseText = "代表者個人補償特約";
     }
   }
